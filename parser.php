4526477f4a43cb871d6a627fccea7e7a9b68da3b
asmi046 <asmi046@gmail.com>
2021-11-28 14:06:29 +0300

    Start

--- parser.php
+++ parser.php
@@ -1,11 +1,11 @@
 <?php
+// php asmi04s9.beget.tech/public_html/parser.php
 header("Content-type: text/html; charset=utf-8");
 ini_set("display_errors",1);
 error_reporting(E_ALL); 
-include_once("../wp-config.php");
 
 
-$xml = simplexml_load_file("MessageFor_0010000000001.xml");
+$xml = simplexml_load_file(__DIR__.'/MessageFor_020000000004.xml');
 
 $ploschadki = $xml->getNamespaces(); 
 $rows = $xml->xpath('//v8msg:Body');
@@ -52,70 +52,55 @@ $objectXML = $rows[0]->xpath('//CatalogObject.РекламныеБлоки');
 $objectList = array();
 
 echo "<pre>";
-	//var_dump($objectXML);
+	var_dump($objectXML);
 echo "</pre>";
 
 
+// global $wpdb;
+// $wpdb->query("TRUNCATE transfet_base");
 
+// $i=1;
 
-global $wpdb;
-$wpdb->query("TRUNCATE transfet_base");
+// foreach ($objectXML as $obj) {
 
-$i=1;
-
-foreach ($objectXML as $obj) {
-
-	$arr["npp"] = $i;
-	$arr["Ref"] = (string)$obj->Ref;
+// 	$arr["npp"] = $i;
+// 	$arr["Ref"] = (string)$obj->Ref;
 	
 	
-	$arr["Description"] = (string)$obj->Description;	
-	$arr["Code"] = (string)$obj->НомерБлока;
+// 	$arr["Description"] = (string)$obj->Description;	
+// 	$arr["Code"] = (string)$obj->НомерБлока;
 	
-	$arr["Type"] = (string)$typeRbLst[(string)$obj->ТипБлока];
-	$arr["Img"] = (string)$obj->ОсновноеИзображение; 
-	$arr["ImgMap"] = (string)$obj->ИзображениеНаКарте;
+// 	$arr["Type"] = (string)$typeRbLst[(string)$obj->ТипБлока];
+// 	$arr["Img"] = (string)$obj->ОсновноеИзображение; 
+// 	$arr["ImgMap"] = (string)$obj->ИзображениеНаКарте;
 	
-	//$arr["Raion"] = (string)$raionLst[(string)$obj->Район];
-	$sep = explode("_",$obj->Описание);
-	$arr["Raion"] = (string)($sep[1]);
+// 	//$arr["Raion"] = (string)$raionLst[(string)$obj->Район];
+// 	$sep = explode("_",$obj->Описание);
+// 	$arr["Raion"] = (string)($sep[1]);
 	
-	$arr["Gorod"] = (string)$gorodLst[(string)$obj->Город];
+// 	$arr["Gorod"] = (string)$gorodLst[(string)$obj->Город];
 	
-	$arr["Opisanie"] = (string)$obj->Описание;
-	$arr["Osveshenie"] = (string)$obj->Освещение;
-	$arr["Koordinati"] = (string)$obj->Координаты;
+// 	$arr["Opisanie"] = (string)$obj->Описание;
+// 	$arr["Osveshenie"] = (string)$obj->Освещение;
+// 	$arr["Koordinati"] = (string)$obj->Координаты;
 	
-	$arr["GRP"] = (string)($sep[0]);
-	$arr["Price"] = $priceLst[(string)$obj->ID];
+// 	$arr["GRP"] = (string)($sep[0]);
+// 	$arr["Price"] = $priceLst[(string)$obj->ID];
 	
-	if (!empty($arr["Koordinati"]))
-		$arr["Koordinati"] = "[".$arr["Koordinati"]."]";
+// 	if (!empty($arr["Koordinati"]))
+// 		$arr["Koordinati"] = "[".$arr["Koordinati"]."]";
 	
-	$objectList[] = $arr;
+// 	$objectList[] = $arr;
 
 
-	global $wpdb;
-	$wpdb->insert(
-		'transfet_base',
-		$arr,
-		array( '%d', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%f' )
-	);
-	$i++;
-}
-
-
-
-
-echo "111";
-echo "<pre>";
-	//var_dump($typeRbLst);
-echo "</pre>";
-
-echo "<pre>";
-	var_dump($objectList);
-echo "</pre>";
-
+// 	global $wpdb;
+// 	$wpdb->insert(
+// 		'transfet_base',
+// 		$arr,
+// 		array( '%d', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%f' )
+// 	);
+// 	$i++;
+// }
 
 
 ?>
